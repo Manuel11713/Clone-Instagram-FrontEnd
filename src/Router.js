28dd27e5a799b5c2e60b0a32951c487b7d7096f5
Manuel Lopez <manuelfx117@gmail.com>
2020-08-22 13:22:00 -0500

    init app

---
+++ src/Router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter,Route,Switch} from 'react-router';
+
+const Router = () =>{
+    <BrowserRouter>
+        
+    </BrowserRouter>
+}
+
+export default Router;
